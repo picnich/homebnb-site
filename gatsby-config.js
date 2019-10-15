40e987f354d81b451ad4c93d3d2815b545c042c0
Nich Pereira <pereira.nich@gmail.com>
2019-10-15 15:04:53 -0400

    included analytics. Moved plugin offline to below manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sitemap`,
-    // `gatsby-plugin-offline`,
     `gatsby-transformer-json`,
     `gatsby-transformer-remark`,
     `gatsby-plugin-eslint`,
@@ -39,26 +38,26 @@ module.exports = {
         display: 'swap'
       }
     },    
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: "UA-60216838-9",
-    //     // Puts tracking script in the head instead of the body
-    //     head: false,
-    //     // Setting this parameter is optional
-    //     // anonymize: true,
-    //     // Setting this parameter is also optional
-    //     // respectDNT: true,
-    //     // Avoids sending pageview hits from custom paths
-    //     // exclude: ["/preview/**", "/do-not-track/me/too/"],
-    //     // Enables Google Optimize using your container Id
-    //     // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-    //     // Any additional create only fields (optional)
-    //     // sampleRate: 5,
-    //     // siteSpeedSampleRate: 10,
-    //     // cookieDomain: "example.com",
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-60216838-9",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        // anonymize: true,
+        // Setting this parameter is also optional
+        // respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Enables Google Optimize using your container Id
+        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Any additional create only fields (optional)
+        // sampleRate: 5,
+        // siteSpeedSampleRate: 10,
+        // cookieDomain: "example.com",
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -70,6 +69,7 @@ module.exports = {
         display: 'minimal-ui',
         icon: 'src/images/HomeBnb-Favicon.png', // This path is relative to the root of the site.
       },
-    }
+    },
+    `gatsby-plugin-offline`
   ],
 };
