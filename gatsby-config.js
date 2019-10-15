8d759aa6d1d535a6fc8799894254e4f7c07e01ab
Nich Pereira <pereira.nich@gmail.com>
2019-10-15 14:57:44 -0400

    commented out analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,26 +39,26 @@ module.exports = {
         display: 'swap'
       }
     },    
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-60216838-9",
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        // anonymize: true,
-        // Setting this parameter is also optional
-        // respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        // exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Enables Google Optimize using your container Id
-        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Any additional create only fields (optional)
-        // sampleRate: 5,
-        // siteSpeedSampleRate: 10,
-        // cookieDomain: "example.com",
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: "UA-60216838-9",
+    //     // Puts tracking script in the head instead of the body
+    //     head: false,
+    //     // Setting this parameter is optional
+    //     // anonymize: true,
+    //     // Setting this parameter is also optional
+    //     // respectDNT: true,
+    //     // Avoids sending pageview hits from custom paths
+    //     // exclude: ["/preview/**", "/do-not-track/me/too/"],
+    //     // Enables Google Optimize using your container Id
+    //     // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+    //     // Any additional create only fields (optional)
+    //     // sampleRate: 5,
+    //     // siteSpeedSampleRate: 10,
+    //     // cookieDomain: "example.com",
+    //   },
+    // },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
